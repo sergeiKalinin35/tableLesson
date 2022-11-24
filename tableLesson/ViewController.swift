3c322a20fa9e89638c4dea7e852a556ba87b604f
sergeiKalinin35 <piolersa85@mail.ru>
2022-11-24 23:53:57 +0200

    Initial Commit

---
+++ tableLesson/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tableLesson
+//
+//  Created by Sergey on 24.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
